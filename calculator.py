84359ccaa0be597ce8ef381d96d5ec4446d649a4
Placement <artem.prokhorov@brushpowergen.com>
2023-10-05 08:35:25 +0100

    Second Commit - Loop

--- calculator.py
+++ calculator.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
+print("\n Welcome to the Calculator v2! \n")
 
-first = float(input("First Number: "))
-second = float(input("Second Number: "))
-action = input("Type one of the following signs: (+) (-) (*) (/) \n\n")
+while True: # Keeps looping until "break"
+    first = float(input("First Number: "))
+    second = float(input("Second Number: "))
+    action = input("Type one of the following signs: (+) (-) (*) (/): ")
 
-if action == "+":
-    result = first + second
-    print(result)
-elif action == "-":
-    result =  first - second
-    print(result)
-elif action == "*":
-    result = first * second
-    print(result)
-elif action == "/":
-    if second != 0.0:
+    if action == "+":
+        result = first + second 
+        print(+ result)
+        break # Exits loop after successful operation
+    elif action == "-":
+        result =  first - second
+        print(result)
+        break # Exits loop after successful operation
+    elif action == "*":
+        result = first * second
+        print(result)
+        break # Exits loop after successful operation
+    elif action == "/":
+        if second == 0.0:
+            print("Error! You can't divide by zero! \n\n")
+            continue # Returns to the beginning of the loop
         result = first / second
         print(result)
+        break # Exits loop after successful operation
     else:
-        print("Error! You can't divide by zero! \n\n")
-else:
-    print("Error! Type one of the following signs: (+) (-) (*) (/) \n\n")
+        print("Error! Type one of the following signs: (+) (-) (*) (/) \n\n")
+
 
