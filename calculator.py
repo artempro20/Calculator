693646f690909f1118a2c1ffbe1cab16ceb5e2c7
Placement <artem.prokhorov@brushpowergen.com>
2023-10-05 10:42:27 +0100

    History + upgraded loop

--- calculator.py
+++ calculator.py
@@ -1,31 +1,43 @@
 #!/usr/bin/env python3
-print("\n Welcome to the Calculator v2! \n")
+print("\n Welcome to the Calculator v3! \n")
+
+history = []
 
 while True: # Keeps looping until "break"
+
+    action = input("Type one of the following signs: (+) (-) (*) (/) or (H): ")
+    
+    if action == "H":
+        for entry in history:
+            print(entry)
+        continue
+
     first = float(input("First Number: "))
     second = float(input("Second Number: "))
-    action = input("Type one of the following signs: (+) (-) (*) (/): ")
+    
 
     if action == "+":
         result = first + second 
-        print(+ result)
-        break # Exits loop after successful operation
+        
+        
     elif action == "-":
         result =  first - second
-        print(result)
-        break # Exits loop after successful operation
+        
+        
     elif action == "*":
         result = first * second
-        print(result)
-        break # Exits loop after successful operation
+        
+        
     elif action == "/":
         if second == 0.0:
             print("Error! You can't divide by zero! \n\n")
             continue # Returns to the beginning of the loop
         result = first / second
-        print(result)
-        break # Exits loop after successful operation
+        
+        
     else:
         print("Error! Type one of the following signs: (+) (-) (*) (/) \n\n")
+        continue
 
-
+    print(result)
+    history.append(f"{first} {action} {second} = {result}")
